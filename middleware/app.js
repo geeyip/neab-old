4d2b98ae411c3745d10c5f3b2f9a35ba097cc217
zhangmengjia <zhangmengjia@hisign.com.cn>
2016-07-19 14:23:02 +0800

    图片上传，base64，修改

--- middleware/app.js
+++ middleware/app.js
@@ -23,8 +23,13 @@ app.set('views', path.join(__dirname, '..', 'api/view'));
 app.set('view engine', 'html');
 
 app.use(favicon(path.join(__dirname, '..','public', 'img', 'favicon.ico')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(session({
